ceb34609c11ca9930ae02b7cc214e17ab97a514b
mo374z <schlager.mo@t-online.de>
2025-03-07 10:46:43 +0100

    updated pyproject toml and add comment to test run script

--- scripts/llm_test_run.py
+++ scripts/llm_test_run.py
@@ -13,89 +13,86 @@ from tqdm import tqdm
 
 logger = Logger(__name__)
 
+# TODO: Align this script with how we import datasets in capo
 
-def main():
-    """Run inference test on a dataset using a specified LLM."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model")
-    parser.add_argument("--output")
-    parser.add_argument("--datasets", default=["agnews", "subj"])
-    parser.add_argument("--token", default=None)
-    parser.add_argument("--batch-size", default=None)
-    parser.add_argument("--revision", default="main")
-    parser.add_argument("--max-model-len", default=None)
-    parser.add_argument("--model-storage-path", default=None)
-    args = parser.parse_args()
 
-    start_time = time.time()
+"""Run inference test on a dataset using a specified LLM."""
+parser = argparse.ArgumentParser()
+parser.add_argument("--model")
+parser.add_argument("--output")
+parser.add_argument("--datasets", default=["agnews", "subj"])
+parser.add_argument("--token", default=None)
+parser.add_argument("--batch-size", default=None)
+parser.add_argument("--revision", default="main")
+parser.add_argument("--max-model-len", default=None)
+parser.add_argument("--model-storage-path", default=None)
+args = parser.parse_args()
 
-    if args.max_model_len is not None:
-        max_model_len = int(args.max_model_len)
+start_time = time.time()
 
-    if "vllm" in args.model:
-        llm = get_llm(
-            args.model,
-            batch_size=args.batch_size,
-            max_model_len=max_model_len,
-            model_storage_path=args.model_storage_path,
-            revision=args.revision,
-        )
-    else:
-        llm = get_llm(args.model, args.token)
+if args.max_model_len is not None:
+    max_model_len = int(args.max_model_len)
 
-    results = pd.DataFrame()
-
-    for dataset in args.datasets:
-        config = Config(
-            evaluation_llm=args.model,
-            ds_path=f"data_sets/cls/{dataset}/",
-            task_name=dataset,
-            api_token=args.token,
-            n_eval_samples=200,
-        )
-
-        task = get_task(config, split="dev")
-        predictor = Classificator(llm, classes=task.classes)
-
-        prompt = task.initial_population
-
-        xs = task.xs[: config.n_eval_samples]
-        ys = task.ys[: config.n_eval_samples]
-
-        for prompt in tqdm(task.initial_population):
-            preds, seqs = predictor.predict(prompt, xs, return_seq=True)
-
-            scores = []
-            for i in range(len(xs)):
-                scores.append(1 if preds[0][i] == ys[i] else 0)
-
-            # clean up the sequences
-            seqs = [seq.replace("\n", "").strip() for seq in seqs]
-
-            # if single prompts should be stored
-            # df = pd.DataFrame(dict(prompt=prompt, seq=seqs, score=scores))
-            # df.to_csv(args.output + "_detailed", index=False)
-
-            accuracy = np.array(scores).mean()
-
-            results = pd.DataFrame(
-                dict(
-                    model=args.model,
-                    dataset=dataset,
-                    prompt=prompt,
-                    accuracy=accuracy,
-                    n_samples=len(xs),
-                ),
-                index=[0],
-            )
-            results.to_csv(args.output, mode="a", header=False, index=False)
-
-    total_inference_time = time.time() - start_time
-    print(
-        f"Total inference took {total_inference_time:.2f} seconds and required {llm.get_token_count()} tokens."
+if "vllm" in args.model:
+    llm = get_llm(
+        args.model,
+        batch_size=args.batch_size,
+        max_model_len=max_model_len,
+        model_storage_path=args.model_storage_path,
+        revision=args.revision,
     )
-    print(f"Results saved to {args.output}")
+else:
+    llm = get_llm(args.model, args.token)
 
+results = pd.DataFrame()
 
-if __name__ == "__main__":
-    main()
+for dataset in args.datasets:
+    config = Config(
+        evaluation_llm=args.model,
+        ds_path=f"data_sets/cls/{dataset}/",
+        task_name=dataset,
+        api_token=args.token,
+        n_eval_samples=200,
+    )
+
+    task = get_task(config, split="dev")
+    predictor = Classificator(llm, classes=task.classes)
+
+    prompt = task.initial_population
+
+    xs = task.xs[: config.n_eval_samples]
+    ys = task.ys[: config.n_eval_samples]
+
+    for prompt in tqdm(task.initial_population):
+        preds, seqs = predictor.predict(prompt, xs, return_seq=True)
+
+        scores = []
+        for i in range(len(xs)):
+            scores.append(1 if preds[0][i] == ys[i] else 0)
+
+        # clean up the sequences
+        seqs = [seq.replace("\n", "").strip() for seq in seqs]
+
+        # if single prompts should be stored
+        # df = pd.DataFrame(dict(prompt=prompt, seq=seqs, score=scores))
+        # df.to_csv(args.output + "_detailed", index=False)
+
+        accuracy = np.array(scores).mean()
+
+        results = pd.DataFrame(
+            dict(
+                model=args.model,
+                dataset=dataset,
+                prompt=prompt,
+                accuracy=accuracy,
+                n_samples=len(xs),
+            ),
+            index=[0],
+        )
+        results.to_csv(args.output, mode="a", header=False, index=False)
+
+total_inference_time = time.time() - start_time
+print(
+    f"Total inference took {total_inference_time:.2f} seconds and required {llm.get_token_count()} tokens."
+)
+print(f"Results saved to {args.output}")
