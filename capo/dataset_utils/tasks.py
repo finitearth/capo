a9a5bb055a3d6545ecb1f3adfe057f7a638d6d7f
Timo Heiß <ti-heiss@t-online.de>
2025-03-12 17:02:45 +0100

    Update and add missing task descriptions

--- capo/dataset_utils/tasks.py
+++ capo/dataset_utils/tasks.py
@@ -1,10 +1,11 @@
 TASK_DESCRIPTIONS = {
-    "sst-5": ...,
-    "agnews": "The dataset contains news articles categorized into four classes: World, Sports, Business, and Tech. The task is to classify each news article into one of the four categories.",
-    "subj": ...,
-    "rte": ...,
-    "gsm8k": "The dataset consists of elementary school math word problems that require multi-step reasoning to solve. The task is to solve each word problem and provide the final answer.",
+    "sst-5": "The dataset consists of movie reviews with five levels of sentiment labels: veryNegative, negative, neutral, positive, and veryPositive. The task is to classify each movie review into one of these five sentiment categories.",
+    "agnews": "The dataset contains news articles categorized into four classes: World, Sports, Business, and Sci/Tech. The task is to classify each news article into one of the four categories.",
+    "subj": "The dataset contains sentences labeled as either subjective or objective. The task is to classify each sentence as either subjective or objective.",
+    "rte": "The dataset contains pairs of sentences where the task is to determine whether the meaning of one sentence can be inferred from the other. The task is to classify each pair as either Entailment (if the second sentence follows logically from the first) or NoEntailment (if the second sentence does not necessarily follow from the first).",
+    "gsm8k": "The dataset consists of grade school math word problems that require multi-step reasoning to solve. The task is to solve each word problem and provide the final answer.",
 }
+
 
 INITIAL_PROMPTS = {
     "sst-5": [...],
