d584bdef3f4e965f46dfc28b9c08bd4e826ff08c
mo374z <schlager.mo@t-online.de>
2025-03-14 09:59:34 +0100

    implement test set evaluation for experiments

--- scripts/evaluate_prompts.py
+++ scripts/evaluate_prompts.py
@@ -1,50 +1,44 @@
 import argparse
+import json
 
 import pandas as pd
 from promptolution.llms import get_llm
-from promptolution.predictors import Classificator
+from promptolution.predictors import MarkerBasedClassificator
 
 from capo.load_datasets import load_dataset
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", required=True)
-parser.add_argument("--csv-path", required=True)
-parser.add_argument("--output-path", required=True)
-parser.add_argument("--dataset", default="SetFit/rte")
-parser.add_argument("--output-dir", default="results/")
-parser.add_argument("--token", default=None)
-parser.add_argument("--batch-size", type=int, default=None)
-parser.add_argument("--revision", default="main")
-parser.add_argument("--max-model-len", type=int, default=2000)
-parser.add_argument("--model-storage-path", default="../models/")
-parser.add_argument("--block-size", type=int, default=30)
-parser.add_argument("--fs-split", type=float, default=0.1)
-parser.add_argument("--n-steps", type=int, default=999)
-parser.add_argument("--n-initial-prompts", type=int, default=10)
-parser.add_argument("--n-eval-samples", type=int, default=300)
+parser.add_argument("--experiment-path", required=True)
 parser.add_argument("--max-tokens", type=int, default=5000000)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    df = pd.read_csv(args.csv_path)
+    # read experiment args
+    with open(f"{args.experiment_path}args.json", "r") as f:
+        experiment_args = json.load(f)
+
+    # read experiment results by using the best prompt per step from the step_results.csv
+    df = pd.read_csv(f"{args.experiment_path}step_results.csv")
+
     # take best per step
-    df = df.groupby("step").apply(lambda x: x.nlargest(1, "score")).reset_index(drop=True)
-    prompts = df["prompt"].tolist()
+    df_results = df.groupby("step").apply(lambda x: x.nlargest(1, "score")).reset_index(drop=True)
+    prompts = df_results["prompt"].unique().tolist()
+
     _, _, test_task = load_dataset(args.dataset, "", val_size=args.n_eval_samples)
     llm = get_llm(
-        args.model,
-        max_model_len=args.max_model_len,
-        batch_size=args.batch_size,
-        model_storage_path=args.model_storage_path,
-        revision=args.revision,
-        token=args.token,
+        model=experiment_args["model"],
+        max_model_len=experiment_args["max_model_len"],
+        batch_size=experiment_args["batch_size"],
+        model_storage_path=experiment_args["model_storage_path"],
+        revision=experiment_args["model_revision"],
     )
 
-    predictor = Classificator(llm, df, test_task.classes)
+    predictor = MarkerBasedClassificator(llm, df, test_task.classes)
 
     scores = test_task.evaluate(prompts, predictor)
 
-    df["test_score"] = scores
-    df.to_csv(args.output_path, index=False)
+    df_results["test_score"] = scores
 
-# do by handing over the path get the configuration
+    # save results to the step_results as extra column by joining on the prompt
+    df = df.join(df_results.set_index("prompt")["test_score"], on="prompt")
+    df.to_csv(f"{args.experiment_path}step_results.csv", index=False)
