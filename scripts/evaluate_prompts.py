d1cea63074a903f84a11e15026573bcf325c1410
mo374z <schlager.mo@t-online.de>
2025-03-14 10:14:08 +0100

    fixes in prompt evaluation

--- scripts/evaluate_prompts.py
+++ scripts/evaluate_prompts.py
@@ -5,10 +5,11 @@ import pandas as pd
 from promptolution.llms import get_llm
 from promptolution.predictors import MarkerBasedClassificator
 
-from capo.load_datasets import load_dataset
+from capo.load_datasets import get_tasks
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--experiment-path", required=True)
+parser.add_argument("--validation-size", type=int, default=500)
 parser.add_argument("--max-tokens", type=int, default=5000000)
 args = parser.parse_args()
 
@@ -24,13 +25,20 @@ if __name__ == "__main__":
     df_results = df.groupby("step").apply(lambda x: x.nlargest(1, "score")).reset_index(drop=True)
     prompts = df_results["prompt"].unique().tolist()
 
-    _, _, test_task = load_dataset(args.dataset, "", val_size=args.n_eval_samples)
+    _, _, test_task = get_tasks(
+        dataset_name=experiment_args["dataset"],
+        optimizer_name=experiment_args["optimizer"],
+        seed=experiment_args["random_seed"],
+        block_size=experiment_args["block_size"],
+    )
+
     llm = get_llm(
-        model=experiment_args["model"],
+        model_id=experiment_args["model"],
         max_model_len=experiment_args["max_model_len"],
         batch_size=experiment_args["batch_size"],
         model_storage_path=experiment_args["model_storage_path"],
         revision=experiment_args["model_revision"],
+        seed=experiment_args["random_seed"],
     )
 
     predictor = MarkerBasedClassificator(llm, df, test_task.classes)
