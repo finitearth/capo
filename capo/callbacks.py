4b9a6108da539d6ee987f80d5d1a92528dc10a77
mo374z <schlager.mo@t-online.de>
2025-03-19 21:56:26 +0100

    analyze racing and change the callback

--- capo/callbacks.py
+++ capo/callbacks.py
@@ -25,12 +25,11 @@ class PickleCallback(Callback):
 
 
 class PromptScoreCallback(Callback):
-    def __init__(self, dir, save_all_steps=False):
+    def __init__(self, dir):
         """Initialize the PromptScoreCallback."""
         if not os.path.exists(dir):
             os.makedirs(dir)
         self.dir = dir
-        self.save_all_steps = save_all_steps
         self.count = 0
 
     def on_step_end(self, optimizer):
@@ -53,13 +52,14 @@ class PromptScoreCallback(Callback):
             for (prompt, block_id), score in eval_dict.items():
                 df.at[prompt, block_id] = score.mean()
 
-            if self.save_all_steps:
-                csv_path = os.path.join(self.dir, f"prompt_scores_{self.count}.csv")
+            csv_path = os.path.join(self.dir, "prompt_scores.csv")
+            df["step"] = self.count
+            if not os.path.exists(csv_path):
                 df.to_csv(csv_path)
-                self.count += 1
             else:
-                csv_path = os.path.join(self.dir, "prompt_scores.csv")
-                df.to_csv(csv_path)
+                df.to_csv(csv_path, mode="a", header=False)
+
+            self.count += 1
 
         return True
 
