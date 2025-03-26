c81b840094b2705950689fac3831193def55957e
finitearth <t.zehle@gmail.com>
2025-03-26 22:16:07 +0100

    fix evaluate init prompts

--- scripts/evaluate_initial_prompts.py
+++ scripts/evaluate_initial_prompts.py
@@ -1,23 +1,24 @@
-import json
 import os
-from glob import glob
 from logging import getLogger
 
 import pandas as pd
 from promptolution.llms import get_llm
 from promptolution.predictors import MarkerBasedClassificator
-from capo.utils import seed_everything
 
-from capo.configs.experiment_configs import llama, qwen, mistral
-
+from capo.configs.experiment_configs import llama, mistral, qwen
 from capo.load_datasets import get_tasks
+from capo.utils import seed_everything
 
 logger = getLogger(__name__)
 
 if __name__ == "__main__":
     datasets = ["agnews", "gsm8k", "subj", "copa", "sst-5"]
-    llms = [(llama.model, llama.revision), (qwen.model, qwen.revision), (mistral.model, mistral.revision)]
-    for revision, llm in llms:       
+    llms = [
+        (llama.model, llama.revision),
+        (qwen.model, qwen.revision),
+        (mistral.model, mistral.revision),
+    ]
+    for llm, revision in llms:
         llm = get_llm(
             model_id=llm,
             max_model_len=2048,
@@ -30,10 +31,7 @@ if __name__ == "__main__":
             seed_everything(42)
 
             _, _, test_task = get_tasks(
-                dataset_name=dataset,
-                optimizer_name="initial",
-                seed=42,
-                block_size=30
+                dataset_name=dataset, optimizer_name="initial", seed=42, block_size=30
             )
 
             predictor = MarkerBasedClassificator(llm=llm, classes=test_task.classes)
@@ -41,15 +39,13 @@ if __name__ == "__main__":
             prompts = test_task.initial_prompts + [
                 "Let's think step by step.",
                 "Let's work this out in a step by step way to be sure we have the right answer.",
-                ""
+                "",
             ]
 
             logger.critical(f"Evaluating {len(prompts)} unique prompts on {dataset} with {llm}")
             scores = test_task.evaluate(prompts, predictor)
 
-            df = pd.DataFrame({
-                "prompt": prompts,
-                "score": scores
-            })
+            df = pd.DataFrame({"prompt": prompts, "score": scores})
 
-            df.to_csv(f"init_results/{dataset}/{llm}/")
+            os.makedirs(f"init_results/{dataset}/{llm}", exist_ok=True)
+            df.to_csv(f"init_results/{dataset}/{llm}/results.csv")
